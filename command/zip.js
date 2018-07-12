9b08768792cef04e19272b1d769b0affa0dbceeb
qiuxia <397401374@qq.com>
2018-07-12 14:58:53 +0800

    gzip & gunzip

--- command/zip.js
+++ command/zip.js
@@ -2,18 +2,30 @@
 const chalk = require('chalk')
 
 module.exports = () => {
-  /**
-   * 压缩
-   */
   var fs = require('fs')
   var zlib = require('zlib')
 
-  var gzip = zlib.createGzip()
+  /**
+   * 压缩
+   */
+  // var gzip = zlib.createGzip()
 
-  var inFile = fs.createReadStream('./extra/compress.txt')
-  var outFile = fs.createWriteStream('./extra/compress.txt.gz')
+  // var inFile = fs.createReadStream('./extra/compress.txt')
+  // var outFile = fs.createWriteStream('./extra/compress.txt.gz')
 
-  inFile.pipe(gzip).pipe(outFile)
+  // inFile.pipe(gzip).pipe(outFile)
 
-  console.log(chalk.green(`\n √ 压缩成功!`))
+  // console.log(chalk.green(`√ 压缩成功!`))
+
+  /**
+   * 解压缩
+   */
+  var gunzip = zlib.createGunzip()
+
+  var inFile = fs.createReadStream('./extra/compress.txt.gz')
+  var outFile = fs.createWriteStream('./extra/compress2.txt')
+
+  inFile.pipe(gunzip).pipe(outFile)
+
+  console.log(chalk.green(`√ 解压成功!`))
 }
