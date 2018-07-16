43f1e94223876e8dcf363d5b04d5cc9194416fd1
qiuxia <397401374@qq.com>
2018-07-16 21:20:31 +0800

    找出文件路径

--- command/fs8.js
+++ command/fs8.js
@@ -5,7 +5,7 @@ const chalk = require('chalk')
 module.exports = () => {
   var fs = require('fs')
 
-  a()
+  b()
 
   /**
    * 找出软链接指向的真实路径
@@ -25,10 +25,33 @@ module.exports = () => {
 
     /**
      * 同步
-     * 注意：不能找硬链接的
+     * 报错`Error: EINVAL: invalid argument, readlink './extra/1for-hard.txt'`：不能找硬链接的
      */
-    const hardRealPath = fs.readlinkSync('./extra/1for-hard.txt')
+    const hardRealPath = fs.readlinkSync('./extra/1for-hard.txt')  // './extra/1for-hard.txt' 为一个硬链接
     console.log('hardRealPath:', hardRealPath)
   }
 
+  /**
+   * 真实路径
+   */
+  function b() {
+    /**
+     * 异步
+     */
+    fs.realpath('./extra/1.txt', (error, realpath) => {
+      if (error) {
+        console.log('查找失败:', error);
+      } else {
+        console.log('查找成功:', realpath);  // 文件路径
+        console.log('process.cwd:', process.cwd());  // 文件所在目录路径
+      }
+    })
+
+    /**
+     * 同步
+     * 报错`Error: ENOENT: no such file or directory, stat '/Users/qiuxia/code/node/node-practice/extra/3for-link.txt'`：不能找软链接
+     */
+    const realPathLink = fs.realpathSync('./extra/3for-link.txt')  // './extra/3for-link.txt' 为一个软链接
+    console.log('realPathLink:', realPathLink)
+  }
 }
