0ec9789a075e320d6e881f3eacea9b6a73cdd5e0
qiuming <397401374@qq.com>
2019-11-06 18:53:31 +0800

    tcp长连接

---
+++ command/http-connection/connection.js
@@ -0,0 +1,39 @@
+/**
+ * tcp 长连接
+ * http请求是在tcp的基础上的，一个tcp可以有多个http请求
+ */
+const http = require('http')
+const fs = require('fs')
+
+module.exports = () => {
+  http.createServer((request, response) => {
+    console.log('request come:', request.url)
+
+    // 读模板
+    const html = fs.readFileSync(`${__dirname}/index.html`, 'utf8')
+    // 读图片
+    const img = fs.readFileSync(`${process.cwd()}/extra/jinmao.jpeg`)
+    console.log('img:', img)
+
+    if (request.url === '/') {
+      // 请求文档资源
+      response.writeHead(200, {
+        'Content-Type': 'text/html',
+        'Connection': 'close' // 不重复使用tcp连接
+      })
+      // 写入模板
+      response.end(html)
+    } else {
+      // 请求图片资源
+      response.writeHead(200, {
+        'Content-Type': 'image/jpeg',
+        'Connection': 'close' // 不重复使用tcp连接
+      })
+      response.end(img)
+    }
+
+    
+  }).listen(8888)
+
+  console.log('server listening on 8888')
+}
