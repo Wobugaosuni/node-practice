b59d6bba2a40ad79a7f38b72ce783d9cc5d312d5
qiuming <397401374@qq.com>
2019-11-08 16:00:07 +0800

    cors

--- command/CORS/server2.js
+++ command/CORS/server2.js
@@ -10,6 +10,8 @@ http.createServer((request, response) => {
     // 请求文档资源
     response.writeHead(200, {
       'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': 'X-Test-Cors',
+      'Access-Control-Max-Age': '600',
     })
     // 写入模板
     response.end('123')
